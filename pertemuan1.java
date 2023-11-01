f4a13d1eedea89a068d01a3c612f7f66b6d8e9a1
RanggaPtr <rangga2000putera@gmail.com>
2023-11-01 19:43:58 +0700

    ADD SOUT

---
+++ pertemuan1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class pertemuan1 {
+    public static void main(String[] args) {
+        Scanner ino = new Scanner (System.in);
+        System.out.println("absdhfbaskfdsadf");
+    }
+}
