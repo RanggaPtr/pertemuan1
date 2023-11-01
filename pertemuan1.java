cc59266bfe0a3992c8c0db17eaac1153b8d72291
RanggaPtr <rangga2000putera@gmail.com>
2023-11-01 20:00:30 +0700

    sout ngasal

--- pertemuan1.java
+++ pertemuan1.java
@@ -4,5 +4,6 @@ public class pertemuan1 {
     public static void main(String[] args) {
         Scanner ino = new Scanner (System.in);
         System.out.println("absdhfbaskfdsadf");
+        System.out.println("bfbasufasfdfihj");
     }
 }
